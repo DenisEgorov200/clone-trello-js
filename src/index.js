e55ff024d8f5366ec82cd4a3f5d955f33f559dd0
DenisEgorov20 <egorov.denis.mem@gmail.com>
2022-08-27 14:36:43 +0300

    github pages

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './index.html';
-import './index.scss';
-import './modules/addBoard';
-import './modules/autoResize';
-import './modules/addTask';
-import './modules/dragAndDrop';
-import './modules/dropDownMenu';
